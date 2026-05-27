069958245f291335d2d703720c7e536b9c6457cc
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-27 12:23:26 -0700

    exercise 5 complete

--- ex05.js
+++ ex05.js
@@ -1,3 +1,6 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [1, 5, 3, 7, 23];
+const containsEven = numbers.some((num) => num % 2 == 0);
+console.log(containsEven ? 'Some of them are even' : 'None of them are even');
