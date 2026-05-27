125a73dd029f169f2a43795860e212c86d9faf1c
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-27 12:14:54 -0700

    exercise 2 complete

--- ex02.js
+++ ex02.js
@@ -1,3 +1,6 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+const numbers = [4, 3, 10, 25, 11, -3];
+const greaterThan10 = numbers.find((num) => num > 10);
+console.log(greaterThan10);
