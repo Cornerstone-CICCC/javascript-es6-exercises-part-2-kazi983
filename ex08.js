0c3db87c514fc9ff6e859fc4f84e07a62cda95b7
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-27 12:34:22 -0700

    exercise 8 complete

--- ex08.js
+++ ex08.js
@@ -1,3 +1,7 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const words = ['We', 'are', 'enjoying', "Elmer's", 'web', 'development', 'course'];
+
+const message = words.join(' ');
+console.log(message);
