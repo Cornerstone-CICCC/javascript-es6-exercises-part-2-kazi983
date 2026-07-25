58b5aebfbe1347c3dfd2c038c5fdf18778833684
Elmer Balbin <contact@elmer.dev>
2026-07-25 09:43:19 +0800

    added feedback

--- ex06.js
+++ ex06.js
@@ -2,7 +2,7 @@
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
 
 const numbers = [5, 24, 10, 7, 0];
-const isEverythingPositive = numbers.every((n) => n >= 0);
+const isEverythingPositive = numbers.every((n) => n >= 0); // Feedback: use n > 0 here; 0 is not a positive number.
 console.log(
   isEverythingPositive ? "There' no negative number" : "There's negative number",
 );
